efdf343976a7027767cc007aef5a4569b1d95180
Sinem Ayyaldaz <sinem.ayyaldaz98@gmail.com>
2022-10-06 21:36:41 +0200

    refresh commit history

---
+++ models.py
@@ -0,0 +1,36 @@
+import uuid
+
+import sqlalchemy as db
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey, UniqueConstraint
+
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    product_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    product_name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
+class Provider(Base):
+    __tablename__ = 'providers'
+
+    provider_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    product = relationship('Product', backref='providers')
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)  # quantity * products.price
+    date = db.Column(db.DateTime, server_default=func.now())
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    customer_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    customer_name = db.Column(db.String(100))
+    customer_email = db.Column(db.String(100), nullable=False)
---
+++ session.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from functools import lru_cache
+from typing import Generator
+
+from config import get_settings
+
+engine = create_engine(get_settings().database_url, pool_pre_ping=True)
+
+
+@lru_cache
+def create_session() -> scoped_session:
+    session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    )
+    return session
+
+
+def get_session() -> Generator[scoped_session, None, None]:
+    session = create_session()
+    try:
+        yield session
+    finally:
+        session.close()
