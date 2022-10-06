0a77af0438da8c9c0a0569f45f6c4615bd3623bc
Sinem Ayyaldaz <sinem.ayyaldaz98@gmail.com>
2022-10-07 00:00:57 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings, PostgresDsn
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    db_name: str
+    db_url: PostgresDsn
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
