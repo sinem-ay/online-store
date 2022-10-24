dff278965c9c3716d4b00246aa505ad0145d5127
sinem-ay <sinem.ayyildiz98@gmail.com>
2022-10-24 11:44:55 +0200

    :recycle: revert provider id type changes

--- schemas/provider.py
+++ schemas/provider.py
@@ -5,15 +5,21 @@ from datetime import datetime
 
 
 class ProviderBase(BaseModel):
-    provider_id: UUID4
+    provider_id: int
     quantity: int
     total_price: float
     date: datetime
 
+    class Config:
+        orm_mode = True
+
 
 class ProviderCreate(ProviderBase):
-    provider_id: UUID4
+    provider_id: int
     product_id: UUID4
     quantity: int
     total_price: float
     date: datetime
+
+    class Config:
+        orm_mode = True
